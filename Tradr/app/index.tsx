b38d357f292bec8ff72006682165ffa72da16c1a
JaydenRaynes <6.jayray@gmail.com>
2024-10-10 20:35:39 -0500

    Added a placeholder data file for some images and modified our index home page for swipe function demo

--- Tradr/app/index.tsx
+++ Tradr/app/index.tsx
@@ -1,12 +1,77 @@
-import { Text, StyleSheet, View, Button, Appearance, useColorScheme } from "react-native";
+import { Text, StyleSheet, View, Button, Appearance, useColorScheme, Image } from "react-native";
 import { useRouter } from "expo-router";
 import { Link, Stack } from 'expo-router';
 import getLocation from './location';
+import Swiper from 'react-native-deck-swiper'; 
+import data from './placeholderimage';
+import React from 'react'
+
+
+const Card = ({ card }) => { //creates my card item that takes the card image from the url listed in data array
+  return(
+  <View style={styles.card}>
+    <Image source={{uri: card.image}} style={styles.cardImage}/>
+  </View>
+  );
+};
 
 export default function Index() {
   const router = useRouter();
   const { longitude, latitude, errorMsg } = getLocation();
+  const [index, setIndex] = React.useState(0);
+  const onSwiped = () => { //Creates the swipe function and changes images in stack 
+    setIndex((index + 1) % data.length);
+  };
   return (
+  <View style={styles.container}>
+    <Swiper
+      cards={data}
+      cardIndex={index}
+      renderCard={(card) => <Card card={card}/>}
+      onSwiper={onSwiped}
+      disableBottomSwipe
+      disableTopSwipe
+      animateOverlayLabelsOpacity //Syling for swipe
+      infinite 
+      overlayLabels={{
+        left: 
+        {title:'Not interested',
+          style:{
+            label:{
+              backgroundColor: 'red',
+              color: 'white',
+              fontSize: 18
+            },
+            wrapper: {
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              justifyContent: 'flex-start',
+              marginTop: 20,
+              marginLeft: -20
+            }
+          }
+        },
+        right:{
+          title:'Interested',
+          style:{
+            label:{
+              backgroundColor: 'green',
+              color: 'white',
+              fontSize: 18
+            },
+            wrapper: {
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              marginTop: 20,
+              marginLeft: 20
+            }
+          }
+
+        }
+      }}
+    r/>    
+  <>
     <View
       style={{
         flex: 1,
@@ -16,6 +81,7 @@ export default function Index() {
       }}
     >
       <Text>This was the Index/Home Screen.</Text>
+    </View>
       <View
         style={{
           flex: 1,
@@ -29,14 +95,39 @@ export default function Index() {
           flex: 1,
           width: "80%",
           height: "80%",
-          backgroundColor: "black,"
         }}
       >
         <Button title="Go to Message Board" onPress={() => router.push("/messageBoard")} />
-        <Button title="Go to Log In" onPress={() => router.push("/loginScreen")} />
+        <Button title="Go to Sign In" onPress={() => router.push("/signIn")} />
         <Button title="Go to Settings" onPress={() => router.push("/settings")} />
       </View>
-    </View>
+      </>
+  </View>
 
   );
 }
+
+const styles = StyleSheet.create({ //Styling to get the card to display on page
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  card:{
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: '#000',
+    shadowOpacity: 0.08,
+    shadowOffset: {width: 0, height: 0},
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  cardImage: {
+    width: '50%',
+    height:'80%',
+    resizeMode: 'contain'
+  }
+  
+});
---
+++ Tradr/app/placeholderimage.jsx
@@ -0,0 +1,12 @@
+const data = [
+    {
+        name: 'Squirtle',
+        image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/EX14/EX14_EN_64.png'
+    },
+    {
+        name: 'Charizard',
+        image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/PGO/PGO_EN_11.png'
+    }
+];
+
+export default data;

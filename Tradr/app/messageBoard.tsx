ff90f20334cc83c25fe0f14e66d2518541be00d1
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2024-10-11 00:07:55 -0500

    Programmed chatWindow to be more of what will be needed in the future using what I learned from the chat.js example code. Final update now that it functions.

--- Tradr/app/messageBoard.tsx
+++ Tradr/app/messageBoard.tsx
@@ -2,15 +2,16 @@ import { Text, View, Button, SafeAreaView, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import React from 'react';
-import Chat from '../chat'
+import ChatWindow from '../chatWindow'
 
 
-const messageBoard = () => {
+const MessageBoard = () => {
     return (
+
         <SafeAreaView style={{ flex: 1, }}>
-            <Chat user={{ _id: 1 }} />
+            <ChatWindow user={{ _userID: 1 }} target={{ _userID: 1 }} />
         </SafeAreaView>
     );
 }
-export default messageBoard;
+export default MessageBoard;
 
---
+++ Tradr/chatWindow.js
@@ -0,0 +1,58 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { GiftedChat, Bubble, InputToolbar, Send } from "react-native-gifted-chat";
+import { FIREBASE_AUTH } from '@/firebase.js';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const { windowHeight } = Dimensions.get('window');
+
+const ChatWindow = ({ user, target }) => {
+
+    const [messages, setMessages] = useState([]);
+    const [senderID, setSenderID] = useState(null);
+    const [recieverID, setRecieverID] = useState(null);
+    // Notes to self for future implementation
+    // Assaign ID when account is created so its always accessable
+    // Assign name to email, or whatever user changes it to in settings
+    // Have an assignable Avatar
+    // Change Later 
+    const setUsers = (sender, reciever) => {
+        setSenderID(1);
+        setRecieverID(2);
+    }
+    // https://github.com/FaridSafi/react-native-gifted-chat Docs for code example
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello, I would like to trade.',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'Trade User',
+                    avatar: null,
+                },
+            },
+        ])
+    }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, messages)
+        );
+    }, []);
+
+    return (
+
+        <GiftedChat
+            messages={messages}
+            onSend={(messages) => onSend(messages)}
+            user={{ _id: user._userID, }}
+            renderUsernameOnMessage={true}
+        />
+
+    )
+}
+
+
+export default ChatWindow;

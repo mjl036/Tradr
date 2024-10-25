9a1e702b8f4ac3c8c479cd8a7417738696cc4a55
mjl036 <mjl036@email.latech.edu>
2024-10-24 19:29:51 -0500

    This will probably fix it.

--- Tradr/app/_layout.tsx
+++ Tradr/app/_layout.tsx
@@ -5,8 +5,8 @@ import { Drawer } from 'expo-router/drawer';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="loginScreen" options={{ title: "Log In" }} />
       <Stack.Screen name="index" options={{ title: "Home Screen" }} />
+      <Stack.Screen name="loginScreen" options={{ title: "Log In" }} />
       <Stack.Screen name="messageBoard" options={{ title: "Message Board" }} />
       <Stack.Screen name="settings" options={{ title: "Settings" }} />
       <Stack.Screen name="accountSettings" options={{ title: "Account Settings" }} />

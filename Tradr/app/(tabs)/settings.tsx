395cf456d4156dcc4dd997cc6ce44e04dea8603e
JaydenRaynes <6.jayray@gmail.com>
2024-11-12 22:20:09 -0600

    Slight changes to slider UI

--- Tradr/app/(tabs)/settings.tsx
+++ Tradr/app/(tabs)/settings.tsx
@@ -1,21 +1,21 @@
 import { Text, Button } from "react-native";
-import React , { Component } from 'react';
+import React , { Component, useState, useContext } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { useColorScheme } from 'react-native';
 import Slider from '@react-native-community/slider';
 import SelectDropdown from 'react-native-select-dropdown';
- 
 
+ 
 // This will most likely be updated in the future to be able to change colors of the light and dark mode and texts
 function darkModeUI() {
-  
   const systemColorScheme = useColorScheme();
   const [theme, setTheme] = React.useState<'light' | 'dark'>(systemColorScheme === 'dark' ? 'dark' : 'light');
-
   const toggleTheme = () => {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
   };
-  
+
+  const [distance, setDistance] = useState(0);
+
   return (
 
     <SafeAreaView style={theme === 'dark' ? styles.darkContainer : styles.lightContainer}>
@@ -24,17 +24,21 @@ function darkModeUI() {
         title="Toggle Notifications"
       />
       <Text>
-      {"{"}slider.value{"}"}
+      {`Distance: ${distance}  miles`}
       </Text>
       <Text style={theme === 'dark' ? styles.darkText : styles.lightText}>
       How far out would you like to see listings?
       </Text>
       <Slider
         style={{width: 350, height: 50}}
-        minimumValue={1}
-        maximumValue={500}
-        minimumTrackTintColor="#FFFFFF"
+        minimumValue={0}
+        maximumValue={1000}
+        step={1}
+        value={distance}
+        minimumTrackTintColor="#0000FF"
         maximumTrackTintColor="#000000"
+        onValueChange={setDistance}
+     
       />
       <Text style={theme === 'dark' ? styles.darkText : styles.lightText}>
       What minimum rating would you like potential matches to be?
@@ -44,7 +48,7 @@ function darkModeUI() {
         minimumValue={0}
         maximumValue={1}
         lowerLimit={0.2}
-        minimumTrackTintColor="#FFFFFF"
+        minimumTrackTintColor="#0000FF"
         maximumTrackTintColor="#000000"
         step={0.2}
         renderStepNumber='true'
